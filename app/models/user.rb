19c3d300108591921bc67b06908ece02cfe623de
anguirado <anguirado@gmail.com>
2012-03-24 20:26:10 -0400

    Done with sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,8 @@ class User < ActiveRecord::Base
 						 :length => {:within => 6..40}
 	before_save :encrypt_password
 
-	 def has_password?(submitted_password)
-    # Compare encrypted_password with the encrypted version of
-    # submitted_password.
+	def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
 	end
 	def self.authenticate(email, submitted_password)
 		user = find_by_email(email)
@@ -50,4 +49,9 @@ class User < ActiveRecord::Base
     def secure_hash(string)
       Digest::SHA2.hexdigest(string)
     end
+	
+	 def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
 end
